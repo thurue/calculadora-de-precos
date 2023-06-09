12fb20aa9b19d70aa4bff8f55c690bdf58fed3bd
Thurue <arthurture8966@gmail.com>
2023-06-09 05:18:39 -0300

    commit one
    teste

---
+++ script.js
@@ -0,0 +1,48 @@
+// .botao{
+//     width: 5vh;
+//     height: 5vh;
+//     position: relative;
+//     top: 50%;
+//     left: 50%;
+//     transform: translate(-50%, -50%);
+
+//     border-radius: 2vh;
+//     background-color: rgb(4, 135, 249, 0);
+//     border: none;
+//     color: white;
+//     -webkit-user-select: none;
+//     transition: filter .2s;
+//     transform: translateZ(1);
+    
+// }
+// .botao:before {
+//     transform: translateZ(0);
+//     background-clip: padding-box;
+//     background-color: #004b6e;
+//     border: solid transparent;
+//     border-radius: 16px;
+//     border-width: 0 0 4px;
+//     bottom: -4px;
+//     content: "";
+//     left: 0;
+//     position: absolute;
+//     right: 0;
+//     top: 0;
+//     z-index: -1;
+// }
+// .botao:hover{
+//     filter: brightness(1.1);
+// }
+
+// button:active {
+//     padding-bottom: 10px;
+// }
+// .MaisMenos{
+//     scale: 1.8;
+// }
+// .mais{
+//     background-color: #1899D6;
+// }
+// .menos{
+//     background-color: rgb(255, 118, 118);
+// }
