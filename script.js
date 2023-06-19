827d62857e79b4ca141ade9a0c4bd09cc122b914
Thurue <arthurture8966@gmail.com>
2023-06-19 18:49:47 -0300

    quase ponto

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
-// .botao{
-//     width: 5vh;
-//     height: 5vh;
-//     position: relative;
-//     top: 50%;
-//     left: 50%;
-//     transform: translate(-50%, -50%);
+var Valortotal = document.getElementById("ValorTotal")
 
-//     border-radius: 2vh;
-//     background-color: rgb(4, 135, 249, 0);
-//     border: none;
-//     color: white;
-//     -webkit-user-select: none;
-//     transition: filter .2s;
-//     transform: translateZ(1);
+var QRefri = 0
+var Coca = document.getElementById("quantidadeRefri")
+
+var Qquentao
+
+function Marefri() {
+    QRefri++
+    Coca.innerText = QRefri
+    Valortotal.innerText = parseFloat(Valortotal.innerText) + 6
+}
+function Merefri() {
+    if (QRefri <= 0) {
+        QRefri = 0
+    }
+    else {
+        QRefri--
+        Coca.innerText = QRefri
+        Valortotal.innerText = parseFloat(Valortotal.innerText) - 6
+    }
+}
+
+function DelAll(){
+    Valortotal.innerText = 0
+    Coca.innerText = 0
+    QRefri = 0 
+
     
-// }
-// .botao:before {
-//     transform: translateZ(0);
-//     background-clip: padding-box;
-//     background-color: #004b6e;
-//     border: solid transparent;
-//     border-radius: 16px;
-//     border-width: 0 0 4px;
-//     bottom: -4px;
-//     content: "";
-//     left: 0;
-//     position: absolute;
-//     right: 0;
-//     top: 0;
-//     z-index: -1;
-// }
-// .botao:hover{
-//     filter: brightness(1.1);
-// }
+}
 
-// button:active {
-//     padding-bottom: 10px;
-// }
-// .MaisMenos{
-//     scale: 1.8;
-// }
-// .mais{
-//     background-color: #1899D6;
-// }
-// .menos{
-//     background-color: rgb(255, 118, 118);
-// }
+
